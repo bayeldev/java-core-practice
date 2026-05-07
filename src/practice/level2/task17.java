b150495c933ad44c0e153c8466921b690014b7f0
BayelDev <bayel.java@gmail.com>
2026-05-08 00:36:19 +0600

    fix num

--- src/practice/level2/task17.java
+++ src/practice/level2/task17.java
@@ -1,7 +1,13 @@
 package practice.level2;
 
+import java.util.Scanner;
+
 public class task17 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+           String pass = sc.nextLine();
+           System.out.println(pass);
+
 
     }
 }
