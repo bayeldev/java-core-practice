a0f92c503e6b5f31ca446ee7943c10971e6455f0
Bayeldev <bayel.java@gmail.com>
2026-05-15 23:09:17 +0600

    solve level 6  task1-8

---
+++ src/practice/level6/task02.java
@@ -0,0 +1,8 @@
+package practice.level6;
+
+public class task02 {
+    public static void main(String[] args) {
+        long dragonHoard=10_000_000_000L;
+        System.out.println(dragonHoard);
+    }
+}
---
+++ src/practice/level6/task04.java
@@ -0,0 +1,12 @@
+package practice.level6;
+
+public class task04 {
+    public static void main(String[] args) {
+        int secretSpellCode=8364;
+        char revealedSymbol =  (char) secretSpellCode;
+        System.out.println("Символ с кодом 8364: " + revealedSymbol);
+        char enigmaticLetter = 'Ж';
+        int numericEssenceOfLetter = enigmaticLetter;
+        System.out.println("Код символа 'Ж': " + numericEssenceOfLetter);
+    }
+}
