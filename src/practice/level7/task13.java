c251e47d40d713c4f3ec840cfc7de90361a0617a
Bayeldev <bayel.java@gmail.com>
2026-05-19 23:10:50 +0600

    finish level 7 task13-20

--- src/practice/level7/task13.java
+++ src/practice/level7/task13.java
@@ -1,4 +1,22 @@
 package practice.level7;
 
 public class task13 {
+    public static void main(String[] args) {
+        int[][] playerAchievements = new int[3][];
+        playerAchievements[0] = new int[] {10, 20};
+        playerAchievements[1] = new int[] {100,200,300,400};
+        playerAchievements[2] = new int[] {1000};
+
+        for(int i = 0; i < playerAchievements.length; i++) {
+            for(int j = 0; j< playerAchievements[i].length; j++) {
+                if(j>0) {
+                    System.out.print(" ");
+                }
+
+                System.out.print(playerAchievements[i][j]);
+
+            }
+            System.out.println();
+        }
+    }
 }
