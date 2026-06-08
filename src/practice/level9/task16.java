e67bba33c23ccd585caf7a1aaa79c62f930677a2
BAYEl <bayel.java@gmail.com>
2026-06-08 20:45:13 +0600

    finish level 9 task 15-20

---
+++ src/practice/level9/task16.java
@@ -0,0 +1,26 @@
+package practice.level9;
+
+
+
+public class task16 {
+    public static void main(String[] args) {
+
+
+        String emailOne = "User@Example.com";
+        String emailTwo = "user@example.com";
+
+        boolean emailEquals= emailOne.equalsIgnoreCase(emailTwo);
+
+        String registrationMessage = "user@example.com is registered";
+
+        boolean isDomen = registrationMessage.contains("example");
+
+        System.out.println(emailEquals + " " + isDomen);
+
+    }
+
+
+
+
+
+}
---
+++ src/practice/level9/task18.java
@@ -0,0 +1,13 @@
+package practice.level9;
+
+public class task18 {
+    public static void main(String[] args) {
+        StringBuilder sent = new StringBuilder("Привет, мир!");
+
+        int index = sent.indexOf(",");
+
+        sent.insert(index+2, "Java ");
+
+        System.out.println(sent);
+    }
+}
